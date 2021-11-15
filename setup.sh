2230a5e04c261ce441ef50a98767853f38c362fa
shreyas0906 <shreyas0906@gmail.com>
2021-11-15 09:23:47 +0530

    update setup.sh
    download the model, unzip to deploy_models

--- setup.sh
+++ setup.sh
@@ -1,5 +1,10 @@
 mkdir -p ~/.streamlit/
 
+mkdir deploy_models
+wget https://drive.google.com/file/d/1QvqNFESW9mJohhGvbiUbSqEvfiJckqsB/view
+unzip pubmed-model.zip -d deploy_models
+
+
 echo "\
 [server]\n \
 headless = true\n\
